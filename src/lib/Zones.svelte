31d6a9c8b7a1112afe9ecbcff10eb382c9624ea2
Eugene Medvedev <Eugene.Medvedev@wwpass.com>
2024-07-31 12:06:49 +0300

    Generalization.

--- src/lib/Zones.svelte
+++ src/lib/Zones.svelte
@@ -2,15 +2,32 @@
     import { GridZone } from "./zone";
     import { zones } from "../geodata/geodata";
 
-    export let grid;
+    export let grid: string | undefined;
+
+    // Produces a list of zone values for each known zonelist.
+    // This way adding new maps can happen automatically.
+    function zoneLoop(g: string) {
+        const zoneTable = [];
+        if (g) {
+            for (const zone of zones) {
+                zoneTable.push({
+                    z: zone,
+                    v: GridZone(zone, g),
+                });
+            }
+        }
+        return zoneTable;
+    }
 </script>
 
 {#if grid}
     <span class="footnote">
         <br />
         <ul class="zones">
-            {#each zones as zone}
-                <li class="nowrap"><b>{zone}</b>: {GridZone(zone, grid)}</li>
+            {#each zoneLoop(grid) as zoneInfo}
+                {#if zoneInfo.v !== null}
+                    <li class="nowrap"><b>{zoneInfo.z}</b>: {zoneInfo.v}</li>
+                {/if}
             {/each}
         </ul>
     </span>
@@ -23,7 +40,6 @@
         list-style: none;
         li {
             display: inline;
-            
         }
         li:after {
             content: ", ";
